c3e037c1e09cb8fc4775fe4a391e89691dbff424
Chree Δ Banon <banon.cf@gmail.com>
2020-10-14 00:40:14 -0400

    Update Database

--- bux/js/api.js
+++ bux/js/api.js
@@ -1,3 +1,15 @@
 window.api = {
-    endpoint: () => { return 'https://api.'+window.app+'.'+tld(); }
+    endpoint: () => { return 'https://api.'+window.app+'.'+tld(); },
+    v1: {
+        create: {
+            video: (stars, id) => {
+                ajax(`https://api.fapbux.localhost/v1/create/xvideos/video/`+stars+`/https://xvideos.com/video`+id+`/`);
+            }
+        },
+        read: {
+            video: (stars, id) => {
+                ajax(`https://api.fapbux.localhost/v1/read/xvideos/video/`+stars+`/https://xvideos.com/video`+id+`/`);
+            }
+        }        
+    }
 }
