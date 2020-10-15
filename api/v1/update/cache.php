fea4a3822368bf354e506095ab3ee59c45ee4e82
Chree Δ Banon <banon.cf@gmail.com>
2020-10-15 17:11:29 -0400

    Update Backend + Database

--- api/v1/update/cache.php
+++ api/v1/update/cache.php
@@ -2,6 +2,25 @@
 $data['params'] = $params; 
 $data['query'] = $query; 
 if($params > 0) {  
+    if($query[0] == "freaks") {
+        $data["DIR"] = $dir = __DIR__."/../../../bux/json";
+        $glob = array_slice(scandir($dir.'/video'),2);
+        $arr = [];
+        $data["freaks"] = [];
+        foreach($glob as $file) {
+            $id = str_replace('.json','',$file);  
+            $rows = json_decode(str_replace('http:','https:',file_get_contents($dir.'/video/'.$file)),true);
+            $freaks = $rows["freaks"];
+            foreach($freaks as $key=>$freak) {
+                $freak = str_replace("%20","_",$freak);
+                $rows["id"] = (int)$id;
+                if(array_key_exists($freak, $data["freaks"])) { array_push($data["freaks"][$freak], $rows); } 
+                else { $data["freaks"][$freak] = [$rows];  }
+            }
+            $data["files"][$freak]["videos"] = $data["freaks"][$freak];
+            file_put_contents($dir.'/freak/'.$freak.'.json',json_encode($data["files"][$freak],JSON_PRETTY_PRINT));
+        }
+    }
     if($query[0] == "videos") {
         $data["DIR"] = $dir = __DIR__."/../../../bux/json";
         $data["glob"] = $glob = array_slice(scandir($dir.'/video'),2);
